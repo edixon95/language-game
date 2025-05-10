7ba24aec1eb32739c3af5462a14a035a372994a4
Elijah Dixon <elijah@ej-software.co.uk>
2025-05-10 13:58:08 +0100

    - add alien - put data where it's actually meant to be

--- lang-game-ts/src/components/UI/GameScreen.tsx
+++ lang-game-ts/src/components/UI/GameScreen.tsx
@@ -1,34 +1,25 @@
-import { useEffect } from "react";
 import { useGameStore } from "../../store/useGameStore";
+import alienImage  from "../../assets/images/alien/alien.png"
 
 export const GameScreen = () => {
     const currentNPCState = useGameStore((s) => s.currentNPCState);
     const options = useGameStore((s) => s.options);
-    const createOptions = useGameStore((s) => s.createOptions);
     const selectOption = useGameStore((s) => s.selectOption);
 
-    //useEffect(() => {
-    //    if (currentNPCState)
-    //        createOptions();
-    //}, [currentNPCState]);
-
     return (
-        <div style={{ display: "flex", border: "1px solid black", width: "80%", flexDirection: "column" }}>
-            <p>{currentNPCState?.dialogueNode.meaning ?? "No dialogue yet"}</p>
+        <div style={{ display: "flex", border: "1px solid black", width: "80%" }}>
             <div style={{
-                marginTop: 20,
-                display: "flex",
-                gap: 10
+                width: "60%"
             }}>
-                {options && options.length > 0 &&
-                    options.map((op, index) => (
-                        <div key={index} style={{ border: "1px solid black", display: "flex", padding: 5, cursor: "pointer" }}
-                            onClick={() => selectOption(index)}
-                        >
-                            <p>{op.meaning}</p>
-                        </div>
-                    ))
-                }
+                Evidence
+            </div>
+            <div style={{
+                width: "40%",
+                borderLeft: "1px solid black",
+                background: `url(${alienImage})`,
+                backgroundSize: "cover", // optional: adjust as needed
+                backgroundPosition: "center"
+            }}>
             </div>
         </div>
     );
--- lang-game-ts/src/components/UI/TextBox.tsx
+++ lang-game-ts/src/components/UI/TextBox.tsx
@@ -1,7 +1,36 @@
+import { useGameStore } from "../../store/useGameStore";
+
 export const TextBox = () => {
+    const currentNPCState = useGameStore((s) => s.currentNPCState);
+    const options = useGameStore((s) => s.options);
+    const selectOption = useGameStore((s) => s.selectOption);
+
     return (
-        <div>
-            <p>T</p>
+        <div style={{ display: "flex", flexDirection: "row", alignItems: "center", width: "100%" }}>
+            <div style={{
+                width: "50%",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center"
+            }}>
+                <p>{currentNPCState?.dialogueNode.meaning ?? "No dialogue yet"}</p>
+            </div>
+            <div style={{
+                display: "flex",
+                gap: 10,
+                justifyContent: "center",
+                width: "50%"
+            }}>
+                {options && options.length > 0 &&
+                    options.map((op, index) => (
+                        <div key={index} style={{ border: "1px solid black", display: "flex", padding: 5, cursor: "pointer" }}
+                            onClick={() => selectOption(index)}
+                        >
+                            <p>{op.meaning}</p>
+                        </div>
+                    ))
+                }
+            </div>
         </div>
     )
 }
